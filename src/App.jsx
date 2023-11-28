848711598574dacd48f7a4dd2c4f75bac90ff846
Polashahmad01 <dev.polashahmad@gmail.com>
2023-11-28 22:28:19 +0600

    Add ProjectsSidebar component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
+import ProjectsSidebar from "./components/ProjectsSidebar"
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <main>
+      <ProjectsSidebar />
+    </main>
   )
 }
 

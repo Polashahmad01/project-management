7df44967c10a7207cc28c5b4cb8771e0cbb7478c
Polashahmad01 <dev.polashahmad@gmail.com>
2023-11-29 21:19:50 +0600

    Add handleSelectProject function and SelectedProject component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,22 @@ import { useState } from "react"
 import ProjectsSidebar from "./components/ProjectsSidebar"
 import NewProject from "./components/NewProject"
 import NoProjectSelected from "./components/NoProjectSelected"
+import SelectedProject from "./components/SelectedProject"
 
 function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
     projects: []
   })
+
+  function handleSelectProject(id) {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: id
+      }
+    })
+  }
 
   function handleStartAddProject() {
     setProjectsState(prevState => {
@@ -44,7 +54,9 @@ function App() {
     })
   }
 
-  let content;
+  const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId)
+
+  let content = <SelectedProject project={selectedProject} />;
 
   if(projectsState.selectedProjectId === null) {
     content = <NewProject onAdd={handleAddProject} onCancel={handleCancelProject}/>
@@ -54,7 +66,7 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar projects={projectsState.projects} onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar projects={projectsState.projects} onStartAddProject={handleStartAddProject} onSelectProject={handleSelectProject} />
       {content}
     </main>
   )
